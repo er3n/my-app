4edc7ff1bb9801d2e9e77ff9e1ef564a11b4a3d0
eren.ozturk <eren.ozturk@localadmins-MacBook-Pro.local>
2019-11-12 11:42:14 +0300

    proxy configuration removed from todo api

--- src/api/todos-api.js
+++ src/api/todos-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = '/api';
+const baseUrl = 'https://simplistic-kick.glitch.me/api';
 
 export const getTodos = () => {
   return axios.get(`${baseUrl}/todos`);
