814d06a761e7a17800031c6c02b71dcc1c5f1811
eren.ozturk <eren.ozturk@localadmins-MacBook-Pro.local>
2019-11-12 13:38:42 +0300

    ws config added

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var expressWs = require('express-ws')(app);
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -17,6 +18,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 let todos = [];
+
+var aWss = expressWs.getWss('/todo');
+
+const sendMessage = (channel, msg) => {
+  aWss.clients.forEach(function(client) {
+    const fullMessage = {
+      channel: channel,
+      body: msg,
+    };
+    client.send(JSON.stringify(fullMessage));
+  });
+};
 
 const addCors = response => {
   const jsonPropertyObject = {
@@ -38,6 +51,7 @@ app.put('/api/todos', function(request, response) {
   todos.push(request.body);
   addCors(response);
   response.sendStatus(200);
+  sendMessage('TodoAdded', request.body);
 });
 
 app.post('/api/todos/:id/complete', function(request, response) {
@@ -48,6 +62,7 @@ app.post('/api/todos/:id/complete', function(request, response) {
     }
   });
   response.sendStatus(200);
+  sendMessage('TodoCompleted', request.params.id);
 });
 
 app.post('/api/todos/:id/redo', function(request, response) {
@@ -58,14 +73,22 @@ app.post('/api/todos/:id/redo', function(request, response) {
     }
   });
   response.sendStatus(200);
+  sendMessage('TodoRedo', request.params.id);
 });
 
 app.delete('/api/todos/:id', function(request, response) {
   const newTodos = todos.filter(item => item.id != request.params.id);
   todos = newTodos;
   response.json(todos);
+  sendMessage('TodoRemoved', request.params.id);
 });
 // listen for requests :)
-const listener = app.listen(process.env.PORT || 3000, function() {
+const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.ws('/ws', function(ws, req) {
+  ws.on('message', function(msg) {
+    ws.send(msg);
+  });
 });
